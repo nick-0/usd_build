022d65f8fc6e431cb5b77793c3ff60beb133af07
nick-0 <nick.christensen@yahoo.com>
2024-03-01 16:07:35 -0500

    Update USD to 24.03

---
+++ build_usd_24.03.bat
@@ -0,0 +1,17 @@
+:: download USD source
+git clone https://github.com/PixarAnimationStudios/USD.git
+:: activate conda
+call conda activate usd_py310
+:: install python packages
+call python -m pip install --upgrade pip
+call python -m pip install PySide2
+call python -m pip install PyOpenGL
+:: use x64 2022
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+:: build USD
+call python USD\build_scripts\build_usd.py D:\dev\usd\usd_24.03_py310 --draco --openimageio --build-variant release
+
+echo import os >> "D:\dev\usd\usd_24.03_py310\lib\python\pxr\__init__.py"
+echo from pathlib import Path >> "D:\dev\usd\usd_24.03_py310\lib\python\pxr\__init__.py"
+echo p = Path(__file__).parents[2] >> "D:\dev\usd\usd_24.03_py310\lib\python\pxr\__init__.py"
+echo os.add_dll_directory(p) >> "D:\dev\usd\usd_24.03_py310\lib\python\pxr\__init__.py"
